70b60db68b192fbeb54a9fa154765b27ea44339e
Ihar Yakimush <IharYakimush@gmail.com>
2019-04-25 18:21:42 +0300

    ingress setup

--- azure/ing-controller.ps1
+++ azure/ing-controller.ps1
@@ -1,2 +1,2 @@
-helm install --name my-release stable/nginx-ingress --set controller.replicaCount=2 --set serviceAccount.create=true --set rbac.create=true
+helm install --name my-release stable/nginx-ingress --set controller.replicaCount=1
 kubectl apply -f nginx-config.yaml
