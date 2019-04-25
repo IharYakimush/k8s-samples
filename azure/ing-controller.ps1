26034c370a8ed7281f0818c1cd0687fa9166440a
Ihar Yakimush <IharYakimush@gmail.com>
2019-04-25 16:44:21 +0300

    es cluster

--- azure/ing-controller.ps1
+++ azure/ing-controller.ps1
@@ -1,1 +1,2 @@
-helm install stable/nginx-ingress --set controller.replicaCount=2
+helm install --name my-release stable/nginx-ingress --set controller.replicaCount=2 --set serviceAccount.create=true --set rbac.create=true
+kubectl apply -f nginx-config.yaml
